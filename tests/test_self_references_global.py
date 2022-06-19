7131dbf2c01315e9ca9b1bb40fc8d784e3810b40
jcal-15 <13785195+jcal-15@users.noreply.github.com>
2022-06-18 19:43:49 -0700

    Testing fix for Python 3.6

--- tests/test_self_references_global.py
+++ tests/test_self_references_global.py
@@ -39,21 +39,21 @@ class DictNode:
 def test_self_ref():
     data = {"name": "n1", "next": {"name": "n2", "next": None}}
     # Can't type check because next is not optional 
-    node = from_dict(LinkListNode, data)
+    node = from_dict(LinkListNode, data, fd_global_ns=globals())
     assert node.name == "n1"
     assert node.next.name == "n2"
 
 
 def test_self_ref_in_list():
     data = {"name": "n1", "children": [{"name": "n2", "children": []}]}
-    node = from_dict(TreeNode, data, fd_check_types=True)
+    node = from_dict(TreeNode, data, fd_check_types=True, fd_global_ns=globals())
     assert node.name == "n1"
     assert node.children[0].name == "n2"
 
 
 def test_self_ref_in_optional():
     data = {"name": "n1", "next": {"name": "n2", "next": None}}
-    node = from_dict(LinkListNode2, data, fd_check_types=True)
+    node = from_dict(LinkListNode2, data, fd_check_types=True, fd_global_ns=globals())
     assert node.name == "n1"
     assert node.next.name == "n2"
 
@@ -66,7 +66,7 @@ def test_self_ref_in_dict():
             "id-456": {"name": "n3", "children": {}}
         }
     }
-    node = from_dict(DictNode, data, fd_check_types=True)
+    node = from_dict(DictNode, data, fd_check_types=True, fd_global_ns=globals())
     assert node.name == "n1"
     assert node.children["id-123"].name == "n2"
     assert node.children["id-456"].name == "n3"
